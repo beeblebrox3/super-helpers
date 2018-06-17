f44408136b692344e41cbc5ae936121e9488f3bd
Luís Henrique <luish.faria@gmail.com>
2018-06-17 18:03:26 -0300

    add indexCollection function

---
+++ src/array/indexCollection.js
@@ -0,0 +1,24 @@
+/**
+ * Takes an array of objects and transforms it into an object where the keys
+ * are some value of each array
+ *
+ * @param {Object[]} data
+ * @param {String} prop
+ * @return {Object}
+ * @memberof array
+ * @name indexCollection
+ *
+ * @example
+ * const data = [{id: 1, name: "foo"}, {id: 2, name: "abc"}]
+ * indexCollection(data, "id")
+ * // output will be {1: {id: 1, name: "foo"}, {id: 2, name: "abc"}}
+ */
+module.exports = function indexCollection (data, prop) {
+    if (!Array.isArray(data)) throw new Error("data should be an array");
+    if (typeof prop !== "string") throw new Error("prop should be a string");
+
+    return data.reduce((response, item) => {
+        response[item[prop]] = item;
+        return response;
+    }, {});
+};
